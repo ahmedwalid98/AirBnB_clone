d784c6eed0e6877234341619f815f749c018e034
Abdulrahman <abdelrahmanmoussa111@gmail.com>
2023-08-12 13:22:33 -0700

    tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Test for storage"""
+
+import unittes
+import json
+from datetime import datetime
+from time import sleep
+from models.engine.file_storage import FileStorage
+
+
+class test_fileStorage(unittest.TestCase):
+    """Test FileStorage Class"""
+    def test_instances(self):
+        """chequeamos instantation"""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
+
+    def test_docs(self):
+        """Test docstrings"""
+        self.assertIsNotNone(FileStorage.all)
+        self.assertIsNotNone(FileStorage.new)
+        self.assertIsNotNone(FileStorage.save)
+        self.assertIsNotNone(FileStorage.reload)
+
+    if __name__ == '__main__':
+        unittest.main()
